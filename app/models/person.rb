30ebe1e1236289cb7c14dcf5740ed55bf33f8b14
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-10-16 16:55:55 -0600

    Remove logs

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,18 @@ class Person
   include Neo4j::ActiveNode
 
   has_one :in, :account, model_class: User, origin: :role
+
+  property :bio
+  property :gender
+  property :website
+  property :hometown
+  property :location
+  property :name, index: :exact
+
+  validates :bio,         presence: true, length: { maximum: 255 }
+  validates :website,     format: URI::regexp(%w(http https))
+  validates :gender,      inclusion: { in: ['male', 'female'] }, allow_nil: true
+  validates :hometown,    length: { maximum: 30 }
+  validates :location,    length: { maximum: 30 }
+  validates :name,        presence: true, length: { maximum: 50  }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
+    username { Faker::Name.name }
+    sequence(:email) { |n| "#{Faker::Internet.email}#{n}" }
+    password { Faker::Internet.password }
   end
 end
