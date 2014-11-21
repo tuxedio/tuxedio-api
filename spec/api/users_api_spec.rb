eab148100ebe14bbacf5861eb1815c1d05249d52
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-11-21 13:49:02 -0700

    Add tests for sessions controller

--- spec/api/users_api_spec.rb
+++ spec/api/users_api_spec.rb
@@ -19,42 +19,34 @@ describe SessionsController do
   describe '#login' do
     it 'responds 200 upon valid authentication with username and password' do
       post login_path, valid_creds, headers_for(:json)
-      expect(response.status).to eq 200
+      expect(response).to have_http_status 200
     end
 
     it 'responds 401 upon invalid authentication with username and password' do
       post login_path, invalid_creds, headers_for(:json)
-      expect(response.status).to eq 401
+      expect(response).to have_http_status 401
     end
 
     it 'responds with authentication token for user' do
       post login_path, valid_creds, headers_for(:json)
-      expect(response_body[:authentication_token]).to_not be_blank
+      expect(response_body[:token]).to_not be_blank
     end
   end
 
   describe '#logout' do
+    it 'responds with 204 status with verified token' do
+      allow(AuthToken).to receive(:valid?)
+        .and_return(true)
+
+      delete logout_path, {}, headers_for(:json)
+      expect(response).to have_http_status 204
+    end
+
     it 'responds with 401 status with invalid credentials' do
-      delete logout_path, user_email: 't@t.com', user_token: '54321',
-        format: :json
-      expect(response.status).to eq 401
-    end
+      headers = headers_for(:json).merge({:Authorization => '12345'})
 
-    it 'responds with 200 status with valid credentials' do
-      delete logout_path, user_email: 't@t.com', user_token: '12345',
-        format: :json
-      expect(response.status).to eq 200
-    end
-
-    it 'removes the authentication token upon logout' do
-      user = create :user, email: 'b@b.com', authentication_token: '12345'
-      delete logout_path, user_email: 'b@b.com', user_token: '12345',
-        format: :json
-
-      delete logout_path, user_email: 'b@b.com', user_token: '12345',
-        format: :json
-
-      expect(response.status).to eq 401
+      delete logout_path, {}, headers
+      expect(response).to have_http_status 401
     end
   end
 end
