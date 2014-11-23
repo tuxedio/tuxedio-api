f4b70a0b4aa2d1a858710c8c5ea2cfad7b5d4d4c
thejinxters <brandon.mikulka@colorado.edu>
2014-11-23 09:01:03 -0700

    Define unique identifier for experience model and add destroy method to experience controller

--- app/controllers/api/v1/experiences_controller.rb
+++ app/controllers/api/v1/experiences_controller.rb
@@ -2,13 +2,25 @@ module Api
   module V1
     class ExperiencesController < ApplicationController
       before_filter :verify_jwt_token
+      before_action :set_v1_experience, only: [:show, :edit, :update, :destroy]
       respond_to :json
+
       def index
         @v1_experiences = Experience.all
-
         respond_with @v1_experiences
-
       end
+
+      def destroy
+        @v1_experience.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_v1_experience
+        @v1_experience = Experience.find(params[:id])
+      end
+
     end
   end
 end
