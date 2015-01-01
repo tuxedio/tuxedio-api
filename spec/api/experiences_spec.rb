4e78d0366b6bf2a84d23967a974da404309b7cf5
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-01-01 18:29:22 -0500

    Add specs to cover #get

--- spec/api/experiences_spec.rb
+++ spec/api/experiences_spec.rb
@@ -1,9 +1,8 @@
 describe Api::V1::ExperiencesController do
-  before { create_list :experience, 10 }
-  after { delete_db }
-
   describe '#index' do
+    before { create_list :experience, 10 }
     before { get v1_experiences_path, headers_for(:json) }
+    after { delete_db }
 
     it 'responds with 200 request (authenticated)' do
       allow(AuthToken).to receive(:valid?).and_return true
@@ -17,6 +16,33 @@ describe Api::V1::ExperiencesController do
 
     it 'responds with the serialized experiences' do
       expect(response_body[:experiences].length).to eq 10
+    end
+  end
+
+  describe '#get' do
+    let!(:exp) { create :experience }
+    before { get v1_experience_path(exp), headers_for(:json) }
+
+    it 'responds with 200 request (authenticated)' do
+      allow(AuthToken).to receive(:valid?).and_return true
+
+      expect(response).to have_http_status 200
+    end
+
+    it 'responds with 200 request (unauthenticated)' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'responds with experience object' do
+      expect(response_body[:experience]).to_not be_nil
+    end
+
+    it 'serializes the attributes of an experience' do
+      attrs_for_experience = %w(id location description name)
+
+      attrs_for_experience.each do |attr|
+        expect(response_body[:experience][attr]).to eq exp.send attr
+      end
     end
   end
 end
