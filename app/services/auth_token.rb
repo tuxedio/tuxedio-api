c52a4bf732e9d1cca3823a08ae08b04837ef1b82
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-01-12 12:59:33 -0700

    Remove expiry constraint on AuthToken

--- app/services/auth_token.rb
+++ app/services/auth_token.rb
@@ -3,21 +3,25 @@ require 'jwt'
 class AuthToken
   attr_reader :token, :payload, :expiry
 
-  def initialize(payload, expiry: 168.hours.from_now)
+  def initialize(payload)
     @payload = payload
     @token = tokenize_payload
-    @expiry = expiry.to_i
   end
 
   def tokenize_payload
-    payload['exp'] = expiry
-    JWT.encode payload, Rails.application.secrets.secret_key_base
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
   def self.valid?(token)
-    JWT.decode token, Rails.application.secrets.secret_key_base
-
+    JWT.decode(token, Rails.application.secrets.secret_key_base)
   rescue
     false
+  end
+
+  def self.user_from_token(token)
+    decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)
+    data = decoded.first { |n| n['user'] }
+    email = data['user']['email']
+    User.find_by email: email
   end
 end
--- spec/services/auth_token_spec.rb
+++ spec/services/auth_token_spec.rb
@@ -1,5 +1,16 @@
 describe AuthToken do
-  let(:payload) { { test: true, data: 'test' } }
+  after { delete_db }
+
+  let(:user) { create :user }
+  let(:payload) do
+    {
+      'user' => {
+        'email' => user.email,
+        'password' => user.password
+      }
+    }
+  end
+
   subject { AuthToken.new payload }
 
   describe '#tokenize_payload' do
@@ -8,10 +19,18 @@ describe AuthToken do
     end
   end
 
-  describe '#valid' do
-    it 'becomes invalid after expiry' do
-      token = AuthToken.new(payload, expiry: 48.hours.ago)
-      expect(AuthToken.valid?(token)).to eq false
+  describe '.valid' do
+    it 'decodes the payload' do
+      expect(AuthToken.valid?(subject.token)).to include payload
+    end
+  end
+
+  describe '.user_from_token' do
+    it 'extracts the user' do
+      found_user = AuthToken.user_from_token(subject.token)
+
+      expect(found_user).to be_a User
+      expect(found_user.email).to eq user.email
     end
   end
 end
