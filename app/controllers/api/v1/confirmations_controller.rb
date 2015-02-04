35c5ef5e7ff890bdb3bd84d4ac990c82506adfe6
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-02-03 19:33:33 -0700

    Render resource on to redirect

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -5,18 +5,18 @@ module Api
         self.resource = User.confirm_by_token params[:confirmation_token]
         yield resource if block_given?
         domain = Rails.application.secrets.domain_name
-        navigate_to_after_confirmation_path resource.errors, domain
+        navigate_to_after_confirmation_path resource, domain
       end
 
       private
 
-      def navigate_to_after_confirmation_path(errors, domain)
-        if errors.empty?
-          respond_with_navigational(resource) do
+      def navigate_to_after_confirmation_path(res, domain)
+        if res.errors.empty?
+          respond_with_navigational res do
             redirect_to domain
           end
         else
-          respond_with_navigational(errors, status: :unprocessable_entity) do
+          respond_with_navigational res.errors, status: :unprocessable_entity do
             redirect_to domain
           end
         end
